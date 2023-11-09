4e1e5d5f021c9ab0b0a5d9e231528cf5efb0e7fa
rejina <rejinakhadka00@gmail.com>
2023-11-09 11:11:24 +0545

    Home page completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import react from "react";
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
+import { getApiConfiguration ,getGenres } from "./store/homeSlice";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./components/header/header";
@@ -19,6 +19,7 @@ function App() {
 
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
   }, []);
   const fetchApiConfig = () => {
     fetchDataFromApi("/configuration").then((res) => {
@@ -33,6 +34,22 @@ function App() {
       dispatch(getApiConfiguration(url));
     });
   };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+         const data = await Promise.all(promises);
+         console.log(data);
+         data.map(({ genres }) => {
+          return genres.map((item) => (allGenres[item.id] = item));
+      });
+      dispatch(getGenres(allGenres));
+  }
   return (
     <BrowserRouter>
     <Header />
