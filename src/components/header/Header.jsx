07a38184a3d72fb01fc56b3581e83a1697fde992
rejina <rejinakhadka00@gmail.com>
2023-11-10 17:03:16 +0545

    Completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -76,7 +76,7 @@ const navigationHandler = (type) => {
   return (
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
-        <div className="logo">
+        <div className="logo" onClick={() =>navigate("/")}>
           <img src={logo} alt=""></img>
         </div>
         <ul className="menuItems">
