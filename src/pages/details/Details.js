baf002581ffad9cb1cac3033a73031193fd6cb3b
Rejina Khadka <rejinakhadka00@gmail.com>
2024-06-17 13:39:20 +0545

    fixed-routingissue

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./style.scss";
 import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
-import DetailsBanner from "./detailsBanner/DetailsBanner";
+
 import Cast from "./cast/Cast";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 const Details = () => {
   const { mediaType, id } = useParams();
